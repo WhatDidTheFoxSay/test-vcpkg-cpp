7ef70baaeb1d37e7c2f3921df2f23ab6cf628344
WhatDidTheFoxSay <2076387176@qq.com>
2024-03-06 13:39:28 +0800

    1.0.0

---
+++ Aspose.Cells/include/Aspose.Cells/UUID.h
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2001-2023 Aspose Pty Ltd. All Rights Reserved.
+// Powered by Aspose.Cells.
+
+#ifndef ASPOSE_CELLS_UUID_H
+#define ASPOSE_CELLS_UUID_H
+
+#ifdef _WIN32
+#include <guiddef.h>
+#else
+#include <uuid/uuid.h>
+#endif
+
+namespace Aspose { namespace Cells {
+	/// <summary>
+	/// Guid struct.
+	/// </summary>
+#ifdef _WIN32
+	typedef GUID UUID;
+#else
+	typedef uuid_t UUID;
+#endif
+} }
+
+#endif
